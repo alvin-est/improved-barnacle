2436a1f1e0217970cf099d5bbeba628495ff7639
alvin-est <estado.alvin@gmail.com>
2024-10-29 00:13:10 +1100

    added starter code

---
+++ index.js
@@ -0,0 +1,18 @@
+/*
+
+This application should:
+ - Prompt the user to select a colour and shape
+ - Provide text for the logo
+ - Save the generated SVG to logo.svg
+
+
+*/
+
+const inquirer = require('inquirer');
+const jest = require('jest');
+
+function init() {
+    console.log("Welcome to the Logo Generator!");
+}
+
+init();
